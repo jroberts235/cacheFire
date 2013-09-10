6e139fb5da73c7f51a032a890b0a86b12aed5513
jroberts235 <jroberts@yeehaaa.com>
2013-09-10 11:52:24 -0700

    forgot to add changes to the main file

--- cacheFire.rb
+++ cacheFire.rb
@@ -1,4 +1,5 @@
-#!/Users/jroberts/.rvm/rubies/jruby-1.7.3/bin/jruby
+#! env jruby
+$LOAD_PATH << './lib'
 
 require 'anemone'
 require 'ruby-progressbar'
@@ -6,129 +7,14 @@ require 'logger'
 require 'persistent_http'
 require 'mixlib/cli'
 require 'java'
-
-# 'java_import' is used to import java classes
-java_import 'java.util.concurrent.Callable'
-java_import 'java.util.concurrent.FutureTask'
-java_import 'java.util.concurrent.LinkedBlockingQueue'
-java_import 'java.util.concurrent.ThreadPoolExecutor'
-java_import 'java.util.concurrent.TimeUnit'
-
-class CacheWarmer 
-  include Mixlib::CLI
-
-  option :url,
-    :short => "-u URL",
-    :long => "--url URL",
-    :description => "URL to access",
-    :required => true
-
-  option :report,
-    :short => "-R",
-    :long => "--Report",
-    :boolean => true,
-    :description => "Count and report the number of links in scour.dat",
-    :default => false
-
-  option :threads,
-    :short => "-t threads",
-    :long => "--threads threads",
-    :description => "Number of parallel threads to use",
-    :default => 1
-
-  option :scour,
-    :short => "-s",
-    :long => "--scour",
-    :boolean => true,
-    :description => "scour URL and build data file?",
-    :default => false
-
-  option :retrieve,
-    :short => "-r",
-    :long => "--retrieve",
-    :boolean => true,
-    :description => "Read the data from scour.dat and pick random URLs to hit",
-    :default => false
-
-  option :pages,
-    :short => "-p pages",
-    :long => "--pages pages",
-    :description => "Number of pages to retrieve",
-    :default => 100
-
-  option :help,
-    :long => "--help",
-    :short => "-h",
-    :description => "Show this message",
-    :on => :tail,
-    :show_options => true,
-    :boolean => true,
-    :exit => 0
-
-  def crawlAndWriteScourFile(url)
-    links = []
-    Anemone.crawl(url) do |anemone|
-      anemone.threads = 15
-    puts "called #{url}"
-      anemone.on_every_page do |page|
-        puts page.links
-        (page.links).each do |link|
-          puts link
-          unless links.include?(link) and link != nil
-            links << link
-            File.open('scour.dat', 'a') do |file| 
-              file << "#{(link.to_s.split('/', 4))[3]}\n"
-            end 
-            $progressbar.increment
-          end
-        end
-      end
-    end
-    puts links
-  end
-  
-  def readScourfile
-    lines = []
-    $log.info("Reading scour.dat")
-    File.open("scour.dat", "r").each_line do |line|
-      lines << line 
-    end
-    puts "Read #{lines.count} lines from dat file"
-    puts "De-duping... #{lines.uniq.count} remaining"
-    $log.info("Read #{lines.count} lines from dat file")
-    $log.info("De-duping... #{lines.uniq.count} remaining")
-    return lines.uniq
-  end
-end # Class end
-
-class Job
-  include Callable
-  def initialize(h,uri)
-    @uri = uri
-    @h   = h
-  end
-  def call
-    @h.request
-    $log.info("getting #{@uri.chomp}")
-    $progressbar.increment
-  end
-end # Class end
-
+require 'cachewarmer.rb'
+require 'counter.rb'     
+require 'job.rb'         
+require 'skip.rb'
 
 begin
 
-  # create a thread pool
-  executor = ThreadPoolExecutor.new(256, # core_pool_treads
-                                    512, # max_pool_threads
-                                    60,  # keep_alive_time
-                                    TimeUnit::SECONDS,
-                                    LinkedBlockingQueue.new)
-
-  # setup logging
-  $log = Logger.new('cacheFire.log', 'daily')
-  $log.datetime_format = "%Y-%m-%d %H:%M:%S"
-
-  # call myclass and parse options
+  # call CacheWarmer and parse options
   cw = CacheWarmer.new
   cw.parse_options
 
@@ -137,24 +23,37 @@ begin
              threads = cw.config[:threads].to_i
   numberOfpagesToGet = cw.config[:pages].to_i
 
+  # create a thread pool
+  executor = ThreadPoolExecutor.new(threads, # core_pool_treads
+                                    256, # max_pool_threads
+                                    5,  # keep_alive_time
+                                    TimeUnit::SECONDS,
+                                    LinkedBlockingQueue.new)
 
-  # crawl and generate scour.dat file if asked to
+  # setup logging
+  $log = Logger.new('cacheFire.log', 'daily')
+  $log.datetime_format = "%Y-%m-%d %H:%M:%S"
+
+
+  # Scour
+  # crawl URL and generate scour.dat file if asked to
   if cw.config[:scour]
     puts "Crawling #{url} looking for links..."
 
-    # some eye candy
+    # some eye candy while you wait
     $progressbar = ProgressBar.create(:starting_at => 20, 
                                       :total => nil)
     cw.crawlAndWriteScourFile(url) 
-    puts "Done!"
+    puts "Done! Now you can run in Retrieve mode."
   end
 
-
+  # Retrieve
   # use the scour.dat file to GET random pages from URL 
+  # URI's will only be loaded once
   if cw.config[:retrieve]
 
-    # some eye candy
-    $progressbar = ProgressBar.create(:format => '%e <%B> %p%% %t',
+    # some eye candy while you wait
+    $progressbar = ProgressBar.create(:format => '%a <%B> %p%% %t',
                                    :starting_at => 0,
                                    :total => numberOfpagesToGet,
                                    :smoothing => 0.8) unless cw.config[:report] 
@@ -168,18 +67,22 @@ begin
 
     h = PersistentHTTP.new(
         :name         => 'cacheFire',
-        :pool_size    => 512,
+        :pool_size    => 1024,
         :pool_timeout => 5,
         :warn_timeout => 0.25,
         :force_retry  => true,
         :url          => url
     )
 
-    tasks = []
+    tasks = [] # array to track threads
+    s = Skip.new # method to track URI's that have been called
+    c = Counter.new # counter method for cache hits and misses
+
     (numberOfpagesToGet/threads).times do
       threads.times do |t|
-        uri = "#{url}#{allLinksFromFile.sample}"
-        task = FutureTask.new(Job.new(h,uri))
+        uri = "#{url}/#{allLinksFromFile.sample}"
+        #next if (s.cached).include?(uri) # skip if hit previously
+        task = FutureTask.new(Job.new(h,uri,c,s))
         executor.execute(task)
         tasks << task
       end
@@ -189,6 +92,10 @@ begin
         t.get
       end
     end
+    $progressbar.finish
+    # report counter stats
+    puts "Cache-Hits: #{c.hits}"
+    puts "Cache-Miss: #{c.total - c.hits}"
   end
 ensure 
   executor.shutdown()
